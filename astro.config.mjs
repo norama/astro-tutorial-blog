c6e038aff65795e6bc540fb6d96fd40e8a0707fe
Nora Mate <norama.matema@gmail.com>
2022-10-31 17:49:38 +0100

    SSR: netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import preact from '@astrojs/preact'
+import preact from '@astrojs/preact';
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   output: 'server',
-})
+  adapter: netlify()
+});
